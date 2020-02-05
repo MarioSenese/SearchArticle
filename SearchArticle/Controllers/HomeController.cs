cd5a83ab0a6b540f7e2aab2d78c424154b20c122
Mario <senese.mario90@gmail.com>
2020-02-05 18:49:10 +0100

    1.0

--- SearchArticle/Controllers/HomeController.cs
+++ SearchArticle/Controllers/HomeController.cs
@@ -6,29 +6,92 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SearchArticle.Models;
 using System.Web;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using log4net;
 
 namespace SearchArticle.Controllers
 {
     public class HomeController : Controller
     {
-       
+        private string keyword;
+        //private Database db = Database.Open("cms|b2b|cmsNEW");
+        private SqlConnection connection = new SqlConnection("Server=SDADMIN45427833/SQL2012R2STD;Database=calzaretta_b2c;User ID=sa;Password=calzaretta!123");
+
+
         // GET: Home
         public ActionResult Index() {
             return View();
         }
 
         [HttpPost]
-        public JsonResult search(Prodotto p) {
+        public JsonResult Search(Prodotto prodotto) => Json(prodotto);
+
+        [HttpPost]
+        public JsonResult SearchArticleCode(Prodotto p) {
             string codice = "";
-            string descrizione = "";
             if (p.codart != null) {
                 codice = p.codart;
             }
-            if(p.descpro != null)
-            {
+            return Json(p);
+        }
+
+        [HttpPost]
+        public JsonResult SearchArticleDescription(Prodotto p) {
+            string descrizione = "";
+            if (p.descpro != null) {
                 descrizione = p.descpro;
             }
             return Json(p);
+        }
+
+        public string HelloWorld()
+        {
+
+            return "Hello World";
+
+        }
+
+        [HttpPost]
+        public string Keyword(string key, string filter)
+        {
+
+            Prodotto p = new Prodotto();
+
+            if (filter.Equals("TextCodice"))
+            {
+                p.codart = key;
+                keyword = p.codart;
+            }
+            else if (filter.Equals("TextDescrizione"))
+            {
+                p.codpro = key;
+                keyword = p.codpro;
+            }
+            else {
+                p.codart = p.codpro = "";
+                keyword = "";
+            }
+           
+            //JsonResult json = Search(p);
+            
+            return keyword;
+
+        }
+
+        [Conditional("DEBUG")]
+        public void ConnectDatabase() {
+
+            try
+            {
+                connection.Open();
+            
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
         }
 
     }
--- SearchArticle/wwwroot/js/search.js
+++ SearchArticle/wwwroot/js/search.js
@@ -1,7 +1,55 @@
-﻿function OnSucces(response) {
-    var keyword = response.keyword;
-}
+﻿$(document).ready(function () {
 
-function OnFailure(response) {
-    alert("Operazione Fallita!!")
+    document.getElementById("progress").hidden = true;
+
+    var submitSearchCode = $("input[type=button].searchCode");
+    var submitSearchDesc = $("input[type=button].searchDesc");
+   
+    $(submitSearchCode).on("click", function (ev) {
+
+        console.log(1);
+        event.preventDefault();
+        
+        var code = $("#TextCodice").val();
+
+        formSubmit(code, "TextCodice")
+        
+    });
+
+    $(submitSearchDesc).on("click", function (ev) {
+
+        console.log(2);
+        event.preventDefault();
+
+        var desc = $("#TextDescrizione").val();
+
+        formSubmit(desc, "TextDescrizione");
+
+    });
+
+});
+
+function formSubmit(keyword, text) {
+
+    document.getElementById("progress").hidden = false;
+
+    $.ajax({
+        url: 'Home/Keyword',
+        type: 'POST',
+        data: { key: keyword, filter: text },
+        dataType: 'text', 
+        success: function (data) {
+
+            console.log(data);
+            document.getElementById("progress").hidden = true;
+
+        },
+        error: function () {
+
+            console.log(data);
+            document.getElementById("progress").hidden = false;
+
+        }
+    });
+
 }
